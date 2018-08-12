0bd4a55d48d5d4a0a772978d189c1e5226c3f57e
Dushan Aluth Patabendige <apdushan@gmail.com>
2018-08-12 13:37:34 +0800

    did refactoring/added user library

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -8,60 +8,15 @@
 
 class Dashboard_model extends CI_Model
 {
-    public function getSubscribers($email, $status, $page = 1)
+    public function getSubscribers($email, $status, $page)
     {
-        /*
-         * Related plain SQL
-         * -----------------
-         * SELECT users.user_id, users.user_first_name, users.user_email, users.user_created, user_statuses.status, user_plans.plan FROM users
-         * INNER JOIN user_statuses ON users.user_status=user_statuses.status_id
-         * INNER JOIN user_plans ON users.user_plan=user_plans.plan_id
-         * WHERE users.user_level=2 AND
-         * user_statuses.status='NEW' AND
-         * users.user_email='amal@dushan.lk';
-         * */
-        if ($page < 1) {
-            $page = 1;
-        }
-        $this->load->database();
-        $this->db->select('users.user_id, users.user_first_name, users.user_email, users.user_created, user_statuses.status, user_plans.plan');
-        $this->db->join('user_statuses', 'users.user_status=user_statuses.status_id', 'inner');
-        $this->db->join('user_plans', 'users.user_plan=user_plans.plan_id', 'inner');
-        $this->db->where("users.user_level", 2);
-
-        if (trim($email) != null && trim($email) != '') {
-            $this->db->where("users.user_email", $email);
-        }
-
-        if (trim($status) != null && trim($status) != '') {
-            $this->db->where("user_statuses.status", $status);
-        }
-
-        $recordsPerPage = 10;
-        $queryOffset = ($page - 1) * $recordsPerPage;
-        /*
-         * Page = 1, OFFSET = 0
-         * Page = 2, OFFSET = 10
-         * Page = 3, OFFSET = 20
-         * Page = 4, OFFSET = 30
-         *
-         * param 2 = num of records
-         * param 3 = OFFSET
-         * */
-        $pages = $this->db->count_all_results('users', FALSE);
-        $this->db->limit($recordsPerPage, $queryOffset);
-        $data = $this->db->get();
-        return array("pages" => $pages / $recordsPerPage, "data" => $data, "currentPage" => $page, "email" => $email, "status" => $status);
+        $this->load->library('user_library');
+        return $this->user_library->subscriber_search($email, $status, $page);
     }
 
     public function getSubscriberProfile($subId)
     {
-        $this->load->database();
-        $this->db->select('users.user_id, users.user_first_name, users.user_last_name, users.user_email, users.	user_company, users.user_created, users.user_last_updated, user_statuses.status, user_plans.plan');
-        $this->db->join('user_statuses', 'users.user_status=user_statuses.status_id', 'inner');
-        $this->db->join('user_plans', 'users.user_plan=user_plans.plan_id', 'inner');
-        $this->db->where("users.user_id", $subId);
-        $data = $this->db->get('users');
-        return array("profile" => $data);
+        $this->load->library('user_library');
+        return $this->user_library->find_subscriber($subId);
     }
 }
--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -10,22 +10,19 @@ class Login_model extends CI_Model
 {
     public function create_subscriber($user)
     {
-        $this->load->database();
-        $this->db->insert('users', $user);
+        $this->load->library('user_library');
+        return $this->user_library->create($user);
     }
 
     public function find_user_by_email($email)
     {
-        $this->load->database();
-        $this->db->where("user_email", $email);
-        return $this->db->get("users");
+        $this->load->library('user_library');
+        return $this->user_library->find_by_email($email);
     }
 
     public function validate_user_credentials($username, $password)
     {
-        $this->load->database();
-        $array = array('user_email' => $username, 'user_password' => $password);
-        $this->db->where($array);
-        return $this->db->get("users");
+        $this->load->library('user_library');
+        return $this->user_library->find_by_email_password($username, $password);
     }
 }
